1e17c0f31e230a768bd5bdd0950010094bd9dd62
Ian Bayne <ianbayne@gmail.com>
2024-02-24 17:18:54 +0900

    refactor: simplify migrations

--- dodgeball-tracker-web/db/migrate/20240223120354_create_hits.rb
+++ dodgeball-tracker-web/db/migrate/20240223120354_create_hits.rb
@@ -1,13 +1,10 @@
 class CreateHits < ActiveRecord::Migration[7.1]
   def change
     create_table :hits do |t|
-      t.references :hitter, null: false
-      t.references :hittee, null: false
+      t.references :hitter, null: false, foreign_key: { to_table: :players }
+      t.references :hittee, null: false, foreign_key: { to_table: :players }
 
       t.timestamps
     end
-
-    add_foreign_key :hits, :players, column: :hitter_id
-    add_foreign_key :hits, :players, column: :hittee_id
   end
 end
--- dodgeball-tracker-web/db/migrate/20240223123246_create_catches.rb
+++ dodgeball-tracker-web/db/migrate/20240223123246_create_catches.rb
@@ -1,13 +1,10 @@
 class CreateCatches < ActiveRecord::Migration[7.1]
   def change
     create_table :catches do |t|
-      t.references :catcher, null: false
-      t.references :catchee, null: false
+      t.references :catcher, null: false, foreign_key: { to_table: :players }
+      t.references :catchee, null: false, foreign_key: { to_table: :players }
 
       t.timestamps
     end
-
-    add_foreign_key :catches, :players, column: :catcher_id
-    add_foreign_key :catches, :players, column: :catchee_id
   end
 end
