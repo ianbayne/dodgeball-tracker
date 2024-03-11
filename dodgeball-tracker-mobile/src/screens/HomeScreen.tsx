e72f6ae19dc3f6269bafc339f20bd364c62b65af
Ian Bayne <ianbayne@gmail.com>
2024-03-11 12:52:59 +0900

    new: add search for players in mobile app

--- dodgeball-tracker-mobile/src/screens/HomeScreen.tsx
+++ dodgeball-tracker-mobile/src/screens/HomeScreen.tsx
@@ -1,6 +1,19 @@
-import { Button, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
-import { gql, useMutation } from "@apollo/client";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
+
+import type Player from "../types/Player";
+
+const SEARCH_PLAYERS = gql`
+  query SearchPlayers($searchTerm: String) {
+    players(search: $searchTerm) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
 
 const CREATE_CATCH = gql`
   mutation CreateCatch($catcherId: ID!, $catcheeId: ID!) {
@@ -35,6 +48,9 @@ const CREATE_HIT = gql`
 `;
 
 function HomeScreen() {
+  const [searchTerm, setSearchTerm] = useState<string>("Enter a player's name");
+
+  const [searchPlayers, { data }] = useLazyQuery(SEARCH_PLAYERS);
   const [createCatch /*, { data, loading, error } */] =
     useMutation(CREATE_CATCH);
   const [createHit /*, { data, loading, error } */] = useMutation(CREATE_HIT);
@@ -51,6 +67,20 @@ function HomeScreen() {
         onPress={() => createHit({ variables: { hitterId: 1, hitteeId: 2 } })}
         title="Create hit"
       />
+      <TextInput
+        value={searchTerm}
+        onChangeText={(text) => {
+          setSearchTerm(text);
+          searchPlayers({ variables: { searchTerm: text } });
+        }}
+      />
+      <View>
+        {data?.players?.map((player: Player) => (
+          <Text key={player.id}>
+            {player.firstName} {player.lastName}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 }
