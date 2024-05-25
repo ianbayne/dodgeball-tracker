31a8db50329e054a8ec456732c1e5ba618e14e50
Ian Bayne <ianbayne@gmail.com>
2024-05-25 18:08:27 +0900

    new: add presence validations

--- dodgeball-tracker-web/app/models/catch.rb
+++ dodgeball-tracker-web/app/models/catch.rb
@@ -4,6 +4,8 @@ class Catch < ApplicationRecord
   belongs_to :catcher, class_name: 'Player'
   belongs_to :catchee, class_name: 'Player'
 
-  validates :catcher, comparison: { other_than: :catchee, message: 'must be a different player than the catchee' }
-  validates :catchee, comparison: { other_than: :catcher, message: 'must be a different player than the catcher' }
+  validates :catcher, presence: true,
+                      comparison: { other_than: :catchee, message: 'must be a different player than the catchee' }
+  validates :catchee, presence: true,
+                      comparison: { other_than: :catcher, message: 'must be a different player than the catcher' }
 end
--- dodgeball-tracker-web/app/models/hit.rb
+++ dodgeball-tracker-web/app/models/hit.rb
@@ -4,6 +4,8 @@ class Hit < ApplicationRecord
   belongs_to :hitter, class_name: 'Player'
   belongs_to :hittee, class_name: 'Player'
 
-  validates :hitter, comparison: { other_than: :hittee, message: 'must be a different player than the hittee' }
-  validates :hittee, comparison: { other_than: :hitter, message: 'must be a different player than the hitter' }
+  validates :hitter, presence: true, 
+                     comparison: { other_than: :hittee, message: 'must be a different player than the hittee' }
+  validates :hittee, presence: true, 
+                     comparison: { other_than: :hitter, message: 'must be a different player than the hitter' }
 end
