15776360b738fd57b1b05cfb63836f36c5a1d500
Ian Bayne <ianbayne@gmail.com>
2024-05-25 18:08:43 +0900

    test: add additional validation tests for hit and catch models

--- dodgeball-tracker-web/spec/models/catch_spec.rb
+++ dodgeball-tracker-web/spec/models/catch_spec.rb
@@ -2,14 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Catch, type: :model do
   describe 'validations' do
-    it 'cannot have the same player as both catcher and catchee' do
+    it 'is only valid with both a catcher and a catchee' do
+      player_1 = create(:player)
+      player_2 = create(:player)
+
+      catch = build(:catch, catcher: nil, catchee: nil)
+
+      expect(catch).not_to be_valid
+
+      catch.catcher = player_1
+      expect(catch).not_to be_valid
+
+      catch.catchee = player_2
+      expect(catch).to be_valid
+    end
+
+    it 'is invalid when both catcher and catchee are same player' do
       player = create(:player)
       catch = build(:catch, catcher: player, catchee: player)
 
-      catch.valid?
-
+      expect(catch).not_to be_valid
       expect(catch.errors.messages[:catcher]).to eq ['must be a different player than the catchee']
       expect(catch.errors.messages[:catchee]).to eq ['must be a different player than the catcher']
+    end
+
+    it 'is valid when catcher and catchee are different players' do
+      player_1 = create(:player)
+      player_2 = create(:player)
+      catch = build(:catch, catcher: player_1, catchee: player_2)
+
+      expect(catch).to be_valid
     end
   end
 end
--- dodgeball-tracker-web/spec/models/hit_spec.rb
+++ dodgeball-tracker-web/spec/models/hit_spec.rb
@@ -2,14 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Hit, type: :model do
   describe 'validations' do
+    it 'is only valid with both a hitter and a hittee' do
+      player_1 = create(:player)
+      player_2 = create(:player)
+
+      hit = build(:hit, hitter: nil, hittee: nil)
+
+      expect(hit).not_to be_valid
+
+      hit.hitter = player_1
+      expect(hit).not_to be_valid
+
+      hit.hittee = player_2
+      expect(hit).to be_valid
+    end
+
+
     it 'cannot have the same player as both hitter and hittee' do
       player = create(:player)
       hit = build(:hit, hitter: player, hittee: player)
 
-      hit.valid?
-
+      expect(hit).not_to be_valid
       expect(hit.errors.messages[:hitter]).to eq ['must be a different player than the hittee']
       expect(hit.errors.messages[:hittee]).to eq ['must be a different player than the hitter']
+    end
+
+    it 'is valid when hitter and hittee are different players' do
+      player_1 = create(:player)
+      player_2 = create(:player)
+      hit = build(:hit, hitter: player_1, hittee: player_2)
+
+      expect(hit).to be_valid
     end
   end
 end
